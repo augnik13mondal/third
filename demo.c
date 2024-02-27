e67e77c69c3e5d823f2c0c499541f2528e1b8532
augnik13mondal <156066320+augnik13mondal@users.noreply.github.com>
2024-02-27 07:25:13 +0530

    Update demo.c

--- demo.c
+++ demo.c
@@ -1,5 +1,5 @@
 int main() {    
-int number1, number2,number3, sum;
+int number1, number2,number3,number, sum;
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
     sum = number1 + number2;      
